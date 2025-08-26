bfea33cd8d773fb248f5f8642b3640245d223eff
Eriko <erikvalencia445@gmail.com>
2025-08-26 02:00:31 -0300

    configuration of add task completed

--- Task-Tracker/task-cli.js
+++ Task-Tracker/task-cli.js
@@ -20,11 +20,30 @@ const args = process.argv.slice(2);
 const command = args[0]
 switch(command){
     case 'add':
-        console.log('Aquí agregaras una terea');
-        brack;
+        const description = args.slince(1).join(" ")//take all the description
+        if(!description){
+            console.log('Please, text a description for the task')
+        }
+
+        const tasks = loadTasks();
+        //create a new task
+        const newTask = {
+            id: tasks.lenght + 1,
+            description: description,
+            status: "todo",
+            createdAt: new Date().toISOString,
+            updatedAt: new Date().toISOString
+        };
+
+        //add the array and save
+        tasks.push(newTask);
+        saveTasks(tasks);
+
+        console.log('Task added correctly (ID: ${newTask.id})')
+        break;
     case 'list':
         console.log('Aquí listarás las tareas');
-        brack;
+        break;
     default:
         console.log('Comando no reconocido');
 }
