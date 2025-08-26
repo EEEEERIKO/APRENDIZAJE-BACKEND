d50c6eb8e94821de57727626847059b15f197457
Eriko <erikvalencia445@gmail.com>
2025-08-26 01:29:52 -0300

    read existing tasks added

--- Task Tracker/task-cli.js
+++ Task Tracker/task-cli.js
@@ -2,3 +2,12 @@ const fs = require('fs');
 const path = './tasks.json';
 
 //read existing tasks
+function loadTasks(){
+    if(!fs.existsSync(path)) {
+        fs.writeFileSync(path, JSON.stringify([]));
+    }
+    const data = fs.readFileSync(path);
+    return JSON.parse(data);
+}
+
+//save tasks
