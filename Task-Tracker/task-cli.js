ea2f98cb5a0d3fb389c3fa616327cb3cf2903b85
Eriko <erikvalencia445@gmail.com>
2025-08-26 02:10:45 -0300

    some fixes of the code

--- Task-Tracker/task-cli.js
+++ Task-Tracker/task-cli.js
@@ -11,7 +11,7 @@ function loadTasks(){
 }
 
 //save tasks
-function saveTasks(){
+function saveTasks(tasks){
     fs.writeFileSync(path, JSON.stringify(tasks, null, 2))
 }
 
@@ -20,7 +20,7 @@ const args = process.argv.slice(2);
 const command = args[0]
 switch(command){
     case 'add':
-        const description = args.slince(1).join(" ")//take all the description
+        const description = args.slice(1).join(" ")//take all the description
         if(!description){
             console.log('Please, text a description for the task')
         }
@@ -28,18 +28,18 @@ switch(command){
         const tasks = loadTasks();
         //create a new task
         const newTask = {
-            id: tasks.lenght + 1,
+            id: tasks.length + 1,
             description: description,
             status: "todo",
-            createdAt: new Date().toISOString,
-            updatedAt: new Date().toISOString
+            createdAt: new Date().toISOString(),
+            updatedAt: new Date().toISOString(),
         };
 
         //add the array and save
         tasks.push(newTask);
         saveTasks(tasks);
 
-        console.log('Task added correctly (ID: ${newTask.id})')
+        console.log(`Task added correctly (ID: ${newTask.id})`)
         break;
     case 'list':
         console.log('Aquí listarás las tareas');
